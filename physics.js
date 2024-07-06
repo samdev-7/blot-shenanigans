dabea6bcf29384667936c1071d8036a4f78af323
Sam Liu <hello@samliu.dev>
2024-07-05 23:10:20 -0400

    Get convex bounding shape of svg

--- physics.js
+++ physics.js
@@ -59,7 +59,7 @@ window.addEventListener("keydown", (e) => {
   let start = Date.now();
   while (true) {
     ticks++;
-    console.log(ticks);
+    // console.log("tick");
     Runner.tick(runner, engine, 1000 / 60);
     let velA = Vector.magnitude(boxA.velocity);
     let velB = Vector.magnitude(boxB.velocity);
@@ -89,4 +89,97 @@ window.addEventListener("keydown", (e) => {
 //   1000
 // );
 
-document.querySelector("svg").querySelector("path");
+// https://stackoverflow.com/questions/25384052/convert-svg-path-d-attribute-to-a-array-of-points
+let pathStr = document
+  .querySelector("svg")
+  .querySelector("path")
+  .getAttribute("d");
+let commands = pathStr.split(/(?=[LMC])/);
+
+let pointArrays = commands.map(function (d) {
+  var pointsArray = d.slice(1, d.length).split(",");
+  var pairsArray = [];
+  for (var i = 0; i < pointsArray.length; i += 2) {
+    pairsArray.push(+pointsArray[i], +pointsArray[i + 1]);
+  }
+  return pairsArray;
+});
+pointArrays.shift();
+
+console.log(pointArrays);
+
+const canvas = document.getElementById("canvas");
+/** @type {CanvasRenderingContext2D} */
+const ctx = canvas.getContext("2d");
+
+pointArrays.forEach((points) => {
+  //   console.log(points);
+  // draw a point at each point
+  ctx.beginPath();
+  ctx.arc(points[0], points[1], 0.1, 0, 2 * Math.PI);
+  ctx.stroke();
+});
+
+// https://github.com/lovasoa/graham-fast/
+function graham_scan(points) {
+  // The enveloppe is the points themselves
+  if (points.length <= 3) return points;
+
+  // Find the pivot
+  var pivot = points[0];
+  for (var i = 0; i < points.length; i++) {
+    if (
+      points[i][1] < pivot[1] ||
+      (points[i][1] === pivot[1] && points[i][0] < pivot[0])
+    )
+      pivot = points[i];
+  }
+
+  // Attribute an angle to the points
+  for (var i = 0; i < points.length; i++) {
+    points[i]._graham_angle = Math.atan2(
+      points[i][1] - pivot[1],
+      points[i][0] - pivot[0]
+    );
+  }
+  points.sort(function (a, b) {
+    return a._graham_angle === b._graham_angle
+      ? a[0] - b[0]
+      : a._graham_angle - b._graham_angle;
+  });
+
+  // Adding points to the result if they "turn left"
+  var result = [points[0]],
+    len = 1;
+  for (var i = 1; i < points.length; i++) {
+    var a = result[len - 2],
+      b = result[len - 1],
+      c = points[i];
+    while (
+      (len === 1 && b[0] === c[0] && b[1] === c[1]) ||
+      (len > 1 &&
+        (b[0] - a[0]) * (c[1] - a[1]) <= (b[1] - a[1]) * (c[0] - a[0]))
+    ) {
+      len--;
+      b = a;
+      a = result[len - 2];
+    }
+    result[len++] = c;
+  }
+  result.length = len;
+  return result;
+}
+
+let boundaryPoints = graham_scan(pointArrays);
+console.log(boundaryPoints);
+
+ctx.strokeStyle = "red";
+
+ctx.beginPath();
+boundaryPoints.forEach((points) => {
+  //   console.log(points);
+  // draw a point at each point
+  ctx.lineTo(points[0], points[1]);
+});
+ctx.closePath();
+ctx.stroke();
